d9b118d87975182493dde6abaf320a1a6d773d30
Young <yangjinbao89757@gmail.com>
2016-11-20 23:39:23 +0800

    s

--- oop.js
+++ oop.js
@@ -1,3 +1,18 @@
 (function (global){
-    function Y()
+    function Y(){
+        this._data = spread(arguments)；
+        function spread(){
+            var res = [];
+            var args = [].slice.call(arguments);
+            args.forEach(function(arg){
+                if(arg instanceof Array){
+                    res.concat(spread(arg));
+                } else{
+                    res.push(arg)
+                }
+            })
+        }
+    }
+    Y.prototype = Array.prototype;
+    global.Y = global.Y || Y;
 })(window)
