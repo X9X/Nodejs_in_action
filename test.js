2689ed7357461fba16f6d3ed2b2a12045c8c8d0f
Young <yangjinbao89757@gmail.com>
2016-11-22 22:35:57 +0800

    add watch mode to webpack

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
-import Foo from './es6/Foo';
-Foo()
+import Person from './es6/Person';
+
+const person = new Person('yang',27,{home:'fy'});
+console.log(Person.isTeen(person.age));
+console.log(person.introSelf());
