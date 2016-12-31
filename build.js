3ea1872af97d69ace87328d3190231e00344b2f4
Young <yangjinbao89757@gmail.com>
2016-12-31 14:09:34 +0800

    some tset

--- build.js
+++ build.js
@@ -1,15 +1,10 @@
 const config = require('./webpack.config');
-console.log(process.argv);
 const webpack = require('webpack');
 
 // returns a Compiler instance
 const compiler = webpack(config);
 // or
-compiler.watch({ // watch options:
-    aggregateTimeout: 300, // wait so long for more changes
-    poll: true // use polling instead of native watchers
-    // pass a number to set the polling interval
-}, function(err, stats) {
+compiler.run(function(err, stats) {
     if (err) { throw new gutil.PluginError('webpack:build', err); }
         console.log('[webpack:build]', stats.toString({
             chunks: false, // Makes the build much quieter
--- test.js
+++ test.js
@@ -1,5 +1,15 @@
 import Person from './es6/Person';
+import Vue from 'vue';
 console.log(2);
 const person = new Person('yang',27,{home:'fy'});
 console.log(Person.isTeen(person.age));
 console.log(person.introSelf());
+
+const vm = new Vue({
+    el : '#app',
+    data : {
+        msg : 'hello'
+    },
+    created:function(){
+    }
+})
