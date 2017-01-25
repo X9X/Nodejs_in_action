d5be4b86558adf1fcb6cfb1a2acd3f010a41d8a4
Young <199681@sh.lianjia.com>
2017-01-25 16:12:04 +0800

    format plus.js

--- other/plus.js
+++ other/plus.js
@@ -1,9 +1,9 @@
 function plus(num, init = 0) {
     let total = num + init;
-    let foo = function (a){
-        return plus(a,total)
+    let foo = function(a) {
+        return plus(a, total)
     }
-    foo.toString = function (){
+    foo.toString = function() {
         return total
     }
     return foo
