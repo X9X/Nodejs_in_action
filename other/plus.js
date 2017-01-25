4df2aa0c194cad9b976c211fa6dee3998c82a9fc
Young <199681@sh.lianjia.com>
2017-01-25 11:57:28 +0800

    add plus.js

---
+++ other/plus.js
@@ -0,0 +1,10 @@
+function plus(num, init = 0) {
+    let total = num + init;
+    let foo = function (a){
+        return plus(a,total)
+    }
+    foo.toString = function (){
+        return total
+    }
+    return foo
+}
