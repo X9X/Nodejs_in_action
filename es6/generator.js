3d7015c1db79fdac979e4621c361a2028e9d4201
Young <yangjinbao02@meituan.com>
2018-10-08 10:12:13 +0800

    add files

--- es6/generator.js
+++ es6/generator.js
@@ -1,1 +1,7 @@
+const axios = require('axios');
 
+function * getPage() {
+  const page1 = yield axios.get('http://www.baidu.com');
+  const page2 = yield axios.get('https://www.google.com');
+  return [page1, page2];
+}
